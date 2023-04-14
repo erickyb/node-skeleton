9b485aea61c46cca82639175280df67530823f33
Edgar Méndez <mdzredgar@gmail.com>
2023-04-13 19:08:51 -0500

    Finalización del controlador.

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -2,6 +2,29 @@ const Users = require('../models/users.models')
 const uuid = require('uuid')
 const { hashPassword } = require('../utils/crypto')
 
+const findAllUsers = async () => {
+  const users = await Users.findAll()
+  return users
+}
+
+const findUserById = async (id) => {
+  const data = await Users.findOne({
+      where: {
+          id: id,
+      }
+  })
+  return data
+}
+
+const findUserByEmail = async (email) => {
+  const data = await Users.findOne({
+      where: {
+          email : email
+      }
+  })
+  return data
+}
+
 const createNewUser = async(userObject) => {
   const newUser = {
     id: uuid.v4(),
@@ -14,4 +37,36 @@ const createNewUser = async(userObject) => {
   }
   const data = await Users.create(newUser)
   return data
+}
+
+const updateUser = async(id, userObj) => {
+
+  const selectedUser = await Users.findOne({
+      where: {
+          id: id
+      }
+  })
+  
+  if(!selectedUser) return null
+
+  const modifiedUser = await selectedUser.update(userObj)
+  return modifiedUser
+}
+
+const deleteUser = async(id) => {
+  const user = await Users.destroy({
+      where: {
+          id: id
+      }
+  })
+  return user // 1 || 0
+}
+
+module.exports = {
+  findAllUsers,
+  findUserById,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  findUserByEmail
 }
